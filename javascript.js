dafce21299576d4bb71c1b0c87f62cea70f758ee
EdgarO93 <87279051+EdgarO93@users.noreply.github.com>
2021-08-06 00:14:07 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,37 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "500px",
+    document.getElementById("box").style.width= "500px";
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor = "blue";
+
+});
+ 
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity = "0.1";
+
+});
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor = "green";
+
+});document.getElementById("button5").addEventListener("click", function(){
+
+    document.getElementById("box").style.border = "solid",
+    document.getElementById("box").style.borderColor="red",
+    document.getElementById("box").style.borderWidth= "32px";
+
+});
+document.getElementById("button6").addEventListener("click", function(){
+
+    document.getElementById("box").style.height= "150px",
+    document.getElementById("box").style.width= "150px",
+    document.getElementById("box").style.backgroundColor= "orange",
+    document.getElementById("box").style.opacity= "1",
+    document.getElementById("box").style.borderWidth= "0px";;
+
+});
